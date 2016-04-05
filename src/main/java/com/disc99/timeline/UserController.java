7cd304651d45bd5c7c030486c127019309c1311f
d.kanehira <disc99.o@gmail.com>
2016-04-05 20:23:37 +0900

    Fix routing

--- src/main/java/com/disc99/timeline/UserController.java
+++ src/main/java/com/disc99/timeline/UserController.java
@@ -1,13 +1,21 @@
 package com.disc99.timeline;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import javax.validation.Valid;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -24,5 +32,23 @@ public class UserController {
         }
         accountRepository.save(new Account(null, request.getUsername(), request.getPassword()));
         return "redirect:/timeline";
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(value = "/debug/users")
+    @ResponseBody
+    public List<Account> a() {
+        return jdbcTemplate.query("select id, name, password from accounts", new RowMapper<Account>() {
+            @Override
+            public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return Account.builder()
+                        .id(new AccountId(rs.getLong("id")))
+                        .name("name")
+                        .password("password")
+                        .build();
+            }
+        });
     }
 }
