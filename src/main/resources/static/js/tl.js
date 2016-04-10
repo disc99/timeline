e24bef39e022ff25b2aa917c3c83ae174673f2f0
d.kanehira <disc99.o@gmail.com>
2016-04-10 14:05:45 +0900

    Add render logic

--- src/main/resources/static/js/tl.js
+++ src/main/resources/static/js/tl.js
@@ -1,10 +1,16 @@
 class ItemStore {
     constructor() {
         this._items = [];
+        this._changeEvents = [];
     }
 
     store(items) {
         this._items = this._items.concat(items);
+        this._changeEvents.forEach(e => e(items));
+    }
+
+    onChange(fn) {
+        this._changeEvents.push(fn);
     }
 
     get items() {
@@ -15,6 +21,7 @@ class ItemStore {
 
 let source = creteEventSource();
 let itemStore = new ItemStore();
+itemStore.onChange(render);
 
 
 document.addEventListener("visibilitychange", function() {
@@ -30,7 +37,7 @@ function creteEventSource() {
         console.log('connected');
     });
     source.addEventListener('message', function (e) {
-        console.log(e.data);
+        console.log("[Catch message] "+e.data);
         fetchItems();
     }, false);
     source.addEventListener('error', function (e) {
@@ -43,15 +50,23 @@ function creteEventSource() {
 }
 
 function fetchItems() {
-    let lastId = itemStore.items.map(i => i.id).sort().reverse()[0] || "";
+    let lastId = itemStore.items.map(i => i.id).sort((a, b) => (parseInt(a) > parseInt(b)) ? 1 : -1).reverse()[0] || "";
     fetch("/timeline/items?lastItemId=" + lastId, {
         credentials: 'include'
     })
     .then(res => res.json())
     .then(json => {
-        console.log(json);
+        console.log("[Fetched JSON] "+JSON.stringify(json.items));
         itemStore.store(json.items);
     });
+}
 
+function render(items) {
+    const ul = document.getElementById("items");
+    items.forEach(i => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(JSON.stringify(i)));
+        ul.appendChild(li);
+    });
 }
 
