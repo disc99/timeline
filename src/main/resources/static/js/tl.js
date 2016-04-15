2b9a0561b1a5b0f6e0dc56cf56b725e9f7a384ff
d.kanehira <disc99.o@gmail.com>
2016-04-15 21:33:15 +0900

    Add IndexedDB

--- src/main/resources/static/js/tl.js
+++ src/main/resources/static/js/tl.js
@@ -2,11 +2,47 @@ class ItemStore {
     constructor() {
         this._items = [];
         this._changeEvents = [];
+
+        let idbReq = indexedDB.open("timeline", 1);
+        this.idbReq.onupgradeneeded = event => {
+            this.store = event.target.result.createObjectStore("items", {keyPath: "id"});
+        };
+        idbReq.onerror = event => console.log("DB open error");
+        idbReq.onsuccess = event => {
+            this.db = idbReq.result;
+
+//            //"twitter", "pocket"2つのオブジェクトストアを読み書き権限付きで使用することを宣言
+//            var transaction = db.transaction(["twitter", "pocket"], "readwrite");
+//
+//            //各オブジェクトストアの取り出し
+//            var twitterStore = transaction.objectStore("twitter");
+//            var pocketStore = transaction.objectStore("pocket");
+//
+//            //twitterオブジェクトストアから全データの取り出し
+//            twitterStore.openCursor().onsuccess = function (event) {
+//                var cursor = event.target.result;
+//                if (cursor) {
+//                    console.log("id_str:" + cursor.key + " Text: " + cursor.value.text);
+//                    cursor.continue();
+//                }
+//            };
+//            //pocketオブジェクトストアからの全データの取り出し
+//            pocketStore.openCursor().onsuccess = function (event) {
+//                var cursor = event.target.result;
+//                if (cursor) {
+//                    console.log("item_id:" + cursor.key + " url: " + cursor.value.url);
+//                    cursor.continue();
+//                }
+//            };
+        }
+
     }
 
     store(items) {
         this._items = this._items.concat(items);
         this._changeEvents.forEach(e => e(items));
+
+
     }
 
     onChange(fn) {
